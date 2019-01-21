7201770a0b5f72391bb8e9d44e04552e75891286
captainozlem <ozlemniksch@gmail.com>
2019-01-20 22:17:59 -0600

    this is the first part

---
+++ src/app.js
@@ -0,0 +1,100 @@
+/* global window */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL, {Marker, Popup, NavigationControl} from 'react-map-gl';
+
+import ControlPanel from './control-panel';
+import CityPin from './city-pin';
+import CityInfo from './city-info';
+
+import CITIES from './cities.json';
+
+const TOKEN =
+  'pk.eyJ1IjoiY2FwdGFpbm96bGVtIiwiYSI6ImNqb2c3bDEycjA2M3gza21nb3oxdDlsYWMifQ.MyssR7nhxDBnbkPzI0wGnA'; // Set your mapbox token here
+
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 37.785164,
+        longitude: -100,
+        zoom: 3.5,
+        bearing: 0,
+        pitch: 0
+      },
+      popupInfo: null
+    };
+  }
+
+  _updateViewport = viewport => {
+    this.setState({viewport});
+  };
+
+  _renderCityMarker = (city, index) => {
+    return (
+      <Marker
+        key={`marker-${index}`}
+        longitude={city.longitude}
+        latitude={city.latitude}
+      >
+        <CityPin size={20} onClick={() => this.setState({popupInfo: city})} />
+      </Marker>
+    );
+  };
+
+  _renderPopup() {
+    const {popupInfo} = this.state;
+
+    return (
+      popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={popupInfo.longitude}
+          latitude={popupInfo.latitude}
+          closeOnClick={false}
+          onClose={() => this.setState({popupInfo: null})}
+        >
+          <CityInfo info={popupInfo} />
+        </Popup>
+      )
+    );
+  }
+
+  render() {
+    const {viewport} = this.state;
+
+    return (
+      <MapGL
+        {...viewport}
+        width="100%"
+        height="100%"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={this._updateViewport}
+        mapboxApiAccessToken={TOKEN}
+      >
+        {CITIES.map(this._renderCityMarker)}
+
+        {this._renderPopup()}
+
+        <div className="nav" style={navStyle}>
+          <NavigationControl onViewportChange={this._updateViewport} />
+        </div>
+
+        <ControlPanel containerComponent={this.props.containerComponent} />
+      </MapGL>
+    );
+  }
+}
+
+export function renderToDom(container) {
+  render(<App />, container);
+}
