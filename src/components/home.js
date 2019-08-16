f0d602a8ffb12f5be040f20cd1c37567c371cb8e
captainozlem <ozlemniksch@gmail.com>
2019-08-16 18:44:07 -0400

    Books are in the position

--- src/components/home.js
+++ src/components/home.js
@@ -3,26 +3,26 @@ import React from 'react';
 export const Home = () => {
   return (
     <div className="home">
-      <div className="book">
-        <div className="back" />
+      <div className="captains_logbook book">
+        <div className="captains_book_back back" />
         <div className="page6" />
         <div className="page5" />
         <div className="page4" />
         <div className="page3" />
         <div className="page2" />
         <div className="page1" />
-        <div className="front">Captain's Log Book</div>
+        <div className="captains_logbook_front front">Captain's Log Book</div>
       </div>
 
-      <div className="book">
-        <div className="back" />
+      <div className="ucarikacari book">
+        <div className="ucarikacari_back back" />
         <div className="page6" />
         <div className="page5" />
         <div className="page4" />
         <div className="page3" />
         <div className="page2" />
         <div className="page1" />
-        <div className="front">Ucarı & Kacarı </div>
+        <div className="ucarikacari_front front"> Uçarı & Kaçarı </div>
       </div>
     </div>
   );
