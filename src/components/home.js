57b42fc02e355fd9d76b6729f393b7dcfdb554c2
captainozlem <ozlemniksch@gmail.com>
2019-09-26 14:10:23 -0400

    style addition in WWC

--- src/components/home.js
+++ src/components/home.js
@@ -16,13 +16,20 @@ export const Home = () => {
 
       <div className="ucarikacari book">
         <div className="ucarikacari_back back" />
-        <div className="page6" />
+        <div className="page6">
+          Heyy Click here and type a lot more text so we can take up as much
+          room as possible
+        </div>
         <div className="page5" />
-        <div className="page4" />
+        <div className="page4"></div>
         <div className="page3" />
         <div className="page2" />
         <div className="page1" />
-        <div className="ucarikacari_front front"> Uçarı & Kaçarı </div>
+        <div className="ucarikacari_front front">
+          {' '}
+          Uçarı & Kaçarı{' '}
+          <img src="https://source.unsplash.com/100x100/?nature,water" />
+        </div>
       </div>
     </div>
   );
